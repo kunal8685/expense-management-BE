d2eda966ea30fa7c7caab6eb5a75dac7ebaede95
kunalms203 <kunal.shinde@austere.co.in>
2026-03-15 13:30:41 +0000

    added dot env

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import dotenv from "dotenv"
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json())
-
-app.get('/',(req,res)=>{
-    res.send("welcome to expense managment syastem")
-})
 
 app.listen(PORT,()=>{
     console.log(`app is listening at port http://localhost:${PORT}`)
