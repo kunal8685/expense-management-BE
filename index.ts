ecee0d588714f5a57f53ad48df358cfc47f61c95
kunalms203 <kunal.shinde@austere.co.in>
2026-03-15 10:33:54 +0000

    setup prisma

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
+const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send("welcome to expense managment syastem")
 })
 
-app.listen(8000,()=>{
-    console.log("app is listening at port 8000")
+app.listen(PORT,()=>{
+    console.log(`app is listening at port http://localhost:${PORT}`)
 })
