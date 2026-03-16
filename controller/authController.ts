085661c3e6b7c9e1114808d5055dc6e6ee44b270
Atharva Bagade <bagadeatharva06@gmail.com>
2026-03-16 23:39:45 +0530

    integrated-login-api

--- controller/authController.ts
+++ controller/authController.ts
@@ -2,6 +2,7 @@ import { type Response, type Request, response } from "express";
 import type { ErrorResponse, SuccessResponse } from "../types/apiRsponse";
 import { prisma } from "../utils/prismaClient";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 export const register = async (
   req: Request,
@@ -40,6 +41,63 @@ export const register = async (
 
   } catch (error) {
     console.error("REGISTER ERROR:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+};
+
+/* ---------------- LOGIN ---------------- */
+
+export const login = async (
+  req: Request,
+  res: Response<ErrorResponse | SuccessResponse<any>>
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        roleId: user.roleId
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: {
+        token,
+        user
+      }
+    });
+
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
 
     return res.status(500).json({
       success: false,
--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controller/authController";
+import { register, login } from "../controller/authController";
 
 const Router = express.Router();
 
 Router.post("/register",register)
 // Router.get("/login",)
+Router.post("/login", login);
 
 export default Router;
