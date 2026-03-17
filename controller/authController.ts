66e1bda7f85ab84e4d93fde3230f5abed1aab67b
kunalms203 <kunalms203@gmail.com>
2026-03-17 16:30:56 +0000

    edited login api

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,24 +1,24 @@
 import { type Response, type Request, response } from "express";
 import type { ErrorResponse, SuccessResponse } from "../types/apiRsponse";
 import { prisma } from "../utils/prismaClient";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const register = async (
   req: Request,
-  res: Response<ErrorResponse | SuccessResponse<any>>
+  res: Response<ErrorResponse | SuccessResponse<any>>,
 ) => {
   try {
     const { name, email, password, role } = req.body;
 
     const existingUser = await prisma.user.findUnique({
-      where: { email }
+      where: { email },
     });
 
     if (existingUser) {
       return res.status(409).json({
         success: false,
-        message: "User with this email already exists"
+        message: "User with this email already exists",
       });
     }
 
@@ -29,42 +29,40 @@ export const register = async (
         name,
         email,
         password: hashedPassword,
-        roleId:role
-      }
+        roleId: role,
+      },
     });
 
     return res.status(201).json({
       success: true,
       message: "User created successfully",
-      data: user
+      data: user,
     });
-
   } catch (error) {
     console.error("REGISTER ERROR:", error);
 
     return res.status(500).json({
       success: false,
-      message: "Internal server error"
+      message: "Internal server error",
     });
   }
 };
 
-
 export const login = async (
   req: Request,
-  res: Response<ErrorResponse | SuccessResponse<any>>
+  res: Response<ErrorResponse | SuccessResponse<any>>,
 ) => {
   try {
     const { email, password } = req.body;
 
     const user = await prisma.user.findUnique({
-      where: { email }
+      where: { email },
     });
 
     if (!user) {
       return res.status(404).json({
         success: false,
-        message: "User not found"
+        message: "User not found",
       });
     }
 
@@ -73,17 +71,17 @@ export const login = async (
     if (!isPasswordValid) {
       return res.status(401).json({
         success: false,
-        message: "Invalid password"
+        message: "Invalid password",
       });
     }
 
     const token = jwt.sign(
       {
         userId: user.id,
-        roleId: user.roleId
+        roleId: user.roleId,
       },
       process.env.JWT_SECRET as string,
-      { expiresIn: "1d" }
+      { expiresIn: "1d" },
     );
 
     return res.status(200).json({
@@ -91,16 +89,15 @@ export const login = async (
       message: "Login successful",
       data: {
         token,
-        user
-      }
+        user: { id:user.id,email: user.email, name: user.name, role: user.roleId },
+      },
     });
-
   } catch (error) {
     console.error("LOGIN ERROR:", error);
 
     return res.status(500).json({
       success: false,
-      message: "Internal server error"
+      message: "Internal server error",
     });
   }
 };
